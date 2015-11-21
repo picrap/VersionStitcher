3faa306d31e0f23db4bfd7f05bf39ed6a0b7aab9
Pascal Craponne <pascal@craponne.fr>
2015-11-21 22:51:40 +0100

    Test for nuget package

--- VersionStitcher/Properties/AssemblyInfo.cs
+++ VersionStitcher/Properties/AssemblyInfo.cs
@@ -1,9 +1,4 @@
-﻿#region SignReferences
-// An automatic tool to presign unsigned dependencies
-// https://github.com/picrap/SignReferences
-#endregion
-
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("VersionStitcher")]
