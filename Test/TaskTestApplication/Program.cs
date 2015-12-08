24df302b9dfc8f171459e91ab860b4cbf6acb2d5
Pascal Craponne <pascal@craponne.fr>
2015-12-08 09:57:20 +0100

    Moved tests to Test/

---
+++ Test/TaskTestApplication/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskTestApplication
+{
+    using System;
+
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, I'm on {Version.CommitShortID} and {this} is not a {Version}");
+            Console.ReadKey();
+        }
+    }
+}
