0e16e55b0d848d9caf647b5542c770ce1b22e71f
Pascal Craponne <pascal@craponne.fr>
2016-07-07 17:33:42 +0200

    Upgraded packages

--- VersionStitcher/VersionStitcherTask.cs
+++ VersionStitcher/VersionStitcherTask.cs
@@ -1,7 +1,20 @@
 ﻿// Version Stitcher - Injects git information at post-build step
 // https://github.com/picrap/VersionStitcher
 // MIT License - http://opensource.org/licenses/MIT
+
+using System;
+
 public class VersionStitcherTask : StitcherTask<VersionStitcher.VersionStitcher>
 {
-    public static int Main(string[] args) => Run(new VersionStitcherTask(), args);
+    public static int Main(string[] args)
+    {
+        try
+        {
+            return Run(new VersionStitcherTask(), args);
+        }
+        catch
+        {
+        }
+        return -1;
+    }
 }
