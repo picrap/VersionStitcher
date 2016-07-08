bc614af88e5e856c7f2116db20256ed7466da2bf
Pascal Craponne <pascal@craponne.fr>
2016-07-08 11:05:58 +0200

    Upgraded packages
    Yes, again.

--- VersionStitcher/VersionStitcherTask.cs
+++ VersionStitcher/VersionStitcherTask.cs
@@ -1,6 +1,8 @@
 ﻿// Version Stitcher - Injects git information at post-build step
 // https://github.com/picrap/VersionStitcher
 // MIT License - http://opensource.org/licenses/MIT
+
+// ReSharper disable once CheckNamespace
 public class VersionStitcherTask : StitcherTask<VersionStitcher.VersionStitcher>
 {
     public static int Main(string[] args) => Run(new VersionStitcherTask(), args);
